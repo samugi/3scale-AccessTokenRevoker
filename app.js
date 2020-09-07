a4c2d294621571ce8008ff16353693c2e96524ab
Kevin Price <kev.price83@gmail.com>
2020-09-07 13:31:31 +0200

    adds new params, removes token get and adds commandline option

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const axios = require('axios');
+const commandLineArgs = require('command-line-args')
 
 //Admin Portal URL
-const apiUrl = ""
-//Access Token with RW access to the Account Management API
-const accessToken = ""
+const optionDefinitions = [
+  { name: 'url', alias: 'u', type: String }
+]
+const options = commandLineArgs(optionDefinitions)
 
 require('body-parser-xml')(bodyParser);
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
-if(apiUrl == "" || accessToken == ""){
-  console.log("Please provide a value for apiUrl and accessToken")
+if(options.url == ""){
+  console.log("Please provide a value for url")
   return;
 }
 
@@ -28,11 +30,11 @@ var server = app.listen(port, ip, function () {
 });
 
 var deleteToken = function(token_id){
-  var uri = apiUrl + '/admin/api/personal/access_tokens/'+token_id+".json";
+  var uri = options.url + '/admin/api/personal/access_tokens/'+token_id+".json";
   console.log(uri);
   axios.delete(uri, {
    data: {
-    access_token: accessToken
+    access_token: token_id
    }
   }).then(response => {
     console.log(JSON.stringify(response.data)); 
@@ -46,27 +48,10 @@ app.get('/', function(req, res){
 
 app.post('/', function(req, res){
    res.set('Content-Type', 'application/json');
-   var token_name = req.body.event.object[0].application[0].extra_fields[0].token_name[0]
-   if(token_name == null || token_name == "")
+   var token_value = req.body.event.object[0].application[0].extra_fields[0].token_value[0]
+   if(token_value == null || token_value == "")
 	return;
-     var uri = apiUrl + '/admin/api/personal/access_tokens.json?name='+token_name;
-     axios.get(uri, {
-      data: {
-       access_token: accessToken
-      }
-     }).then(response => {
-       var tokens = response.data.access_tokens
-       if(tokens.length == 0){
-         console.log("no tokens found")
-	 return;
-       }
-       tokens.forEach(function(token){
-	 var token_id = token.access_token.id
-         deleteToken(token_id);
-      })}
-     )
-   res.sendStatus(200)
-   console.log(JSON.stringify(token_name))
+  deleteToken(token_value);
 });
 
 module.exports = app;

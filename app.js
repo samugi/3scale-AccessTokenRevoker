4400d17291dd9113ac6e07abf6c3df053efeca73
Kevin Price <kev.price83@gmail.com>
2020-09-07 15:27:23 +0200

    adds code comments for demo branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 const axios = require('axios');
 const commandLineArgs = require('command-line-args')
 
-//Admin Portal URL
+//commandline argument provides API Manager URL
 const optionDefinitions = [
   { name: 'url', alias: 'u', type: String }
 ]
@@ -14,7 +14,7 @@ require('body-parser-xml')(bodyParser);
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 if(options.url == ""){
-  console.log("Please provide a value for url")
+  console.log("Please provide a value for the API manager url")
   return;
 }
 
@@ -29,6 +29,7 @@ var server = app.listen(port, ip, function () {
   console.log('listening on port ' + port );
 });
 
+//Personal access token delete API call
 var deleteToken = function(token_id){
   var uri = options.url + '/admin/api/personal/access_tokens/'+token_id+".json";
   console.log(uri);
@@ -46,6 +47,7 @@ app.get('/', function(req, res){
   //incoming get requests to be handled here
 });
 
+//inbound webhooks from API manager
 app.post('/', function(req, res){
    res.set('Content-Type', 'application/json');
    var token_value = req.body.event.object[0].application[0].extra_fields[0].token_value[0]
